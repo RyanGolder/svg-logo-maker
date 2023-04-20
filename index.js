636a2c5d088a6a51c29e09603f0383232c2f085e
Ryan Golder <rgolder78@gmail.com>
2023-04-20 18:38:37 +1000

    Updating index.js and shapes.js to form questions and shape

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const fs = require('fs');
+
+const generateShape = require("./lib/shapes");
+
+const shapeOptions = ["circle", "triangle", "square"];
+
+const questions = [
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to 3 characters for your logo e.g. RWG",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message:
+      "Enter a color you'd like your text to be e.g. White, Black, or Blue",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Please choose a shape from the below list.",
+    choices: shapeOptions,
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter a color you'd like your shape to be.",
+  },
+];
+
+// Function to write svg file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName),data)
+}
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const svgLogoContent = generateShape(answers);
+        writeToFile("logo.svg", svgLogoContent)
+    })
+}
+
+// Function call to initialize app
+init();
